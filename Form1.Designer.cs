2af985ff95683e6117ee88474581358766a26ae2
Alessandro Lion <alessandro.lion@outlook.com>
2021-02-07 23:30:38 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿
+namespace ALSolutionCenter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1ToolBox = new System.Windows.Forms.Button();
+            this.button2PDF = new System.Windows.Forms.Button();
+            this.button3JPG = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.comboBoxHPPrinter = new System.Windows.Forms.ComboBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1ToolBox
+            // 
+            this.button1ToolBox.Location = new System.Drawing.Point(62, 89);
+            this.button1ToolBox.Name = "button1ToolBox";
+            this.button1ToolBox.Size = new System.Drawing.Size(124, 76);
+            this.button1ToolBox.TabIndex = 0;
+            this.button1ToolBox.Text = "ToolBox";
+            this.button1ToolBox.UseVisualStyleBackColor = true;
+            this.button1ToolBox.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2PDF
+            // 
+            this.button2PDF.Location = new System.Drawing.Point(243, 89);
+            this.button2PDF.Name = "button2PDF";
+            this.button2PDF.Size = new System.Drawing.Size(124, 76);
+            this.button2PDF.TabIndex = 1;
+            this.button2PDF.Text = "Scan PDF";
+            this.button2PDF.UseVisualStyleBackColor = true;
+            this.button2PDF.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3JPG
+            // 
+            this.button3JPG.Location = new System.Drawing.Point(243, 188);
+            this.button3JPG.Name = "button3JPG";
+            this.button3JPG.Size = new System.Drawing.Size(124, 76);
+            this.button3JPG.TabIndex = 2;
+            this.button3JPG.Text = "Scan JPG";
+            this.button3JPG.UseVisualStyleBackColor = true;
+            this.button3JPG.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(62, 188);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(124, 76);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "HP Guided Solution 2";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(300, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "2021 FOSS ( Free Open Source Software ) from Alex \"Pi\" Lion";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel1.Controls.Add(this.linkLabel1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(9, 270);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(412, 89);
+            this.panel1.TabIndex = 5;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(107, 64);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(191, 13);
+            this.linkLabel1.TabIndex = 6;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "https://alexpilion.wordpress.com/links/";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(317, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Foss without fuss, just working. If it doesn\'t feel fre to let me know.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(0, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(10, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = ".";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Location = new System.Drawing.Point(9, 365);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(412, 22);
+            this.panel2.TabIndex = 7;
+            // 
+            // comboBoxHPPrinter
+            // 
+            this.comboBoxHPPrinter.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxHPPrinter.FormattingEnabled = true;
+            this.comboBoxHPPrinter.Location = new System.Drawing.Point(56, 17);
+            this.comboBoxHPPrinter.Name = "comboBoxHPPrinter";
+            this.comboBoxHPPrinter.Size = new System.Drawing.Size(332, 21);
+            this.comboBoxHPPrinter.TabIndex = 8;
+            this.comboBoxHPPrinter.SelectedIndexChanged += new System.EventHandler(this.comboBoxHPPrinter_SelectedIndexChanged);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Controls.Add(this.comboBoxHPPrinter);
+            this.panel3.Location = new System.Drawing.Point(9, 13);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(412, 58);
+            this.panel3.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Printer";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(433, 394);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3JPG);
+            this.Controls.Add(this.button2PDF);
+            this.Controls.Add(this.button1ToolBox);
+            this.Name = "Form1";
+            this.Text = "Alex Lion Solution Center for HP DeskJet F2280";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1ToolBox;
+        private System.Windows.Forms.Button button2PDF;
+        private System.Windows.Forms.Button button3JPG;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ComboBox comboBoxHPPrinter;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.Drawing.Printing;
+using System.Windows.Forms;
+
+namespace ALSolutionCenter
+{
+    public partial class Form1 : Form
+    {
+        private string sActionToolBox;
+        private string sActionScan;
+        private string sActionGuidedSol;
+        private string sSelectedPrinter;
+        private string sSelectedPrinterCUE;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            App_Starting(label3, comboBoxHPPrinter);
+        }
+
+        static void App_Starting(Label reflabel, ComboBox refcomboBox)
+        {
+            PopulateInstalledHPPrintersCombo(refcomboBox);
+        }
+
+        static void PopulateInstalledHPPrintersCombo(ComboBox MyHPPrinterCombo)
+        {
+            // Add list of installed printers found to the combo box.
+            // The pkInstalledPrinters string will be used to provide the display string.
+            String pkInstalledPrinters;
+            for (int i = 0; i < PrinterSettings.InstalledPrinters.Count; i++)
+            {
+                pkInstalledPrinters = PrinterSettings.InstalledPrinters[i];
+                if (pkInstalledPrinters.IndexOf("HP ") > -1)
+                {
+                    MyHPPrinterCombo.Items.Add(pkInstalledPrinters);
+                }
+            }
+            if (MyHPPrinterCombo.Items.Count > 0)
+            {
+                MyHPPrinterCombo.SelectedIndex = 0;
+            }
+        }
+
+        private void comboBoxHPPrinter_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            sSelectedPrinter = comboBoxHPPrinter.SelectedItem.ToString();
+            PrepareButtons();
+        }
+
+        private void PrepareButtons()
+        {
+            // "Computer\HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Print\Printers\HP Deskjet F2200 series\PrinterDriverData"
+            const string printersRoot = "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Print\\Printers\\";
+            const string printerToolBox = "C:\\Program Files (x86)\\HP\\Digital Imaging\\bin\\hpqtbx01.exe";
+            const string printerGuidedSol = "C:\\Program Files (x86)\\HP\\Digital Imaging\\bin\\hpqgplgt02.exe";
+            const string printerscanexe = "C:\\Program Files (x86)\\HP\\Digital Imaging\\bin\\hpqkygrp.exe";
+
+            string skeyName = printersRoot + "\\" + sSelectedPrinter + "\\PrinterDriverData";
+
+
+            /*
+             * TODO: New func, check if the driver is installed on the canonical path and eventually fix the path in the sAction variabiles accordingly to the 
+            real position
+
+             * 
+             * Computer\HKEY_LOCAL_MACHINE\SOFTWARE\Hewlett-Packard\Models
+             * Keyname= HP Deskjet F2200 series 
+             * Value= C:\Program Files (x86)\HP\Digital Imaging\{60D6AAC5-FDC1-49BA-867B-3135F4726156}\
+             *  */
+
+            /*
+             * ??? NOT IMPLEMENTED BUTTON
+             * "C:\Program Files (x86)\HP\Digital Imaging\bin\hpqkygrp.exe" -n"#Hewlett-Packard#HP Deskjet F2200 series#1602513960" -aSCAN -fSCAN_TMA
+            * */
+
+            sSelectedPrinterCUE = Registry.GetValue(skeyName, "SoftwareCUEContextID", "").ToString();
+
+            label3.Text = "Preparing Buttons for " + sSelectedPrinterCUE;
+
+            sActionToolBox = printerToolBox;
+            sActionGuidedSol = printerGuidedSol;
+            sActionScan = printerscanexe;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Process.Start(sActionToolBox, "\"" + sSelectedPrinter + "\"");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Process.Start(sActionGuidedSol, sSelectedPrinter);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Process.Start(sActionScan, "-aSCAN -fSCAN_DOCUMENT -n\"" + sSelectedPrinterCUE + "\"");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Process.Start(sActionScan, "-aSCAN -fSCAN_PICTURE -n\"" + sSelectedPrinterCUE + "\"");
+        }
+    }
+}
